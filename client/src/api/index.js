9e1b198a4c8aa3b132d7bc56075109577be55c77
Priyanshu <priyanshubansal35@gmail.com>
2023-07-12 12:50:34 +0530

    Added .env file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-// const url = 'http://192.168.1.3:5000/posts';
-const url = 'https://social-media-website-backend.onrender.com/posts';
+const url = process.env.REACT_APP_BASE_URL;
+
+console.log(url);
 
 export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url,newPost);
-export const updatePost = (id,newPost) => axios.patch(`${url}/${id}`,newPost);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, newPost) => axios.patch(`${url}/${id}`, newPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`)
 
 //To fetch data from the above urls
